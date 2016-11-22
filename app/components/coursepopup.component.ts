76b57ab8e76fb8a588b3d6f0b8dd4ad0af019ee4
aaronzhang95 <aaronz@live.unc.edu>
2016-11-21 19:47:40 -0500

    changed div

--- app/components/coursepopup.component.ts
+++ app/components/coursepopup.component.ts
@@ -12,9 +12,9 @@ import { CourseModel } from '../models/course.model';
     <div class="modal-footer">
       <a href="javascript:void(0)" (click)="hide()" class="modal-close waves-effect waves-green btn-flat">Close</a>
     </div>
-    
-    <div class="modal-overlay open" id="materialize-modal-overlay-1" style="z-index: 1002; display: block; opacity: 0.5;"></div>
   </div>
+
+  <div *ngIf="show" class="modal-overlay open" id="materialize-modal-overlay-1" style="z-index: 1002; display: block; opacity: 0.5;"></div>
 	`
 })
 export class CoursePopupComponent {
