ab3245c0f6001c2d50e80bd512b3f515a6b01ee6
John Espenhahn <john@espenhahn.org>
2016-11-26 23:25:31 -0500

    Course details popup at bottom

--- app/components/coursepopup.component.ts
+++ app/components/coursepopup.component.ts
@@ -1,33 +1,40 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CourseModel } from '../models/course.model';
 
 @Component({
     selector: 'coursepopup',
     template: `
-  <div class="modal open">
+  <div class="modal bottom-sheet" [class.open]="open">
     <div class="modal-content">
       <h4>Details on {{ course?.code }}</h4>
       <p>{{ course?.desc }}</p>
     </div>
     <div class="modal-footer">
-      <a href="javascript:void(0)" (click)="onClickClose()" class="modal-close waves-effect waves-green btn-flat">Close</a>
+      <a href="javascript:void(0)" (click)="onClickClose()" class="modal-action modal-close btn-flat">
+        Close
+      </a>
     </div>
   </div>
 
-  <div class="modal-overlay open" id="materialize-modal-overlay-1" (click)="onClickClose()"></div>
+  <div class="modal-overlay" [class.open]="open" (click)="onClickClose()"></div>
 	`,
   styleUrls: [ 'app/components/coursepopup.component.css' ]
 })
-export class CoursePopupComponent {
+export class CoursePopupComponent implements OnInit {
 
-  @Output()
+  private open = false;
   close = new EventEmitter();
   
   @Input()
   course: CourseModel;
   
+  ngOnInit() {
+    setTimeout(() => { this.open = true }, 50);
+  }
+  
   onClickClose() {
-    this.close.emit();
+    this.open = false;
+    setTimeout(() => { this.close.emit() }, 500);
   }
   
 }
