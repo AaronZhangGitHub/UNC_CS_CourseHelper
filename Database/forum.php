85483ed01b4e4e6e36beed9a8ebac08830116ac0
Brennan Proudfoot <bwproud@live.unc.edu>
2016-12-02 03:01:38 -0500

    Added forum server that will act as the controller between the view(forum that Aaron is creating) and the model(the database), depending on the url, it interacts in different ways with the ORM. Has options to Create, Read, Update, Or Delete both posts and comments.

---
+++ Database/forum.php
@@ -0,0 +1,282 @@
+<?php
+require_once('./orm.php');
+$path=$_SERVER['REQUEST_URI'];
+$path=substr($path, strpos($path, ".php")+4);
+$path_components = explode('/', $path);
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+   if (sizeof($path_components)>=4 && $path_components[3]!=""&& $path_components[2]!=""&& $path_components[1]!="") {
+      $coid= intval($path_components[3]);
+      $comment = Comment::findByCOID($coid);
+
+      if ($comment == null) {
+        header("HTTP/1.0 404 Not Found");
+        print("comment id: " . $coid . " not found.");
+        exit();
+      }
+
+      header("Content-type: application/json");
+      print($comment->getJSON());
+      exit();
+   }else if (sizeof($path_components)>=3 && $path_components[2]!=""&& $path_components[1]!="") {
+      $pid= intval($path_components[2]);
+      $post = Post::findByPID($pid);
+
+      if ($post == null) {
+        header("HTTP/1.0 404 Not Found");
+        print("post id: " . $pid . " not found.");
+        exit();
+      }
+
+      header("Content-type: application/json");
+      print($post->getJSON());
+      exit();
+  }else if(sizeof($path_components)>=2 && $path_components[1]!=""){
+      $cid= intval($path_components[1]);
+      $posts = Post::findByCID($cid);
+
+      if ($posts == null) {
+        header("HTTP/1.0 404 Not Found");
+        print("posts id: " . $cid . " not found.");
+        exit();
+      }
+
+      header("Content-type: application/json");
+      print($posts);
+      exit();
+  }
+} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  if (sizeof($path_components)>=3 && $path_components[2]!=""&& $path_components[1]!="") {
+      $pid= intval($path_components[2]);
+
+      if ($post == null) {
+        header("HTTP/1.0 404 Not Found");
+        print("post id: " . $pid . " not found.");
+        exit();
+      }
+
+      if (!isset($_REQUEST['uid'])) {
+        header("HTTP/1.0 400 Bad Request");
+        print("Missing user");
+        exit();
+      }
+      
+      $uid = trim($_REQUEST['uid']);
+      if ($uid == "") {
+        header("HTTP/1.0 400 Bad Request");
+        print("Bad user");
+        exit();
+      }
+
+      if (!isset($_REQUEST['text'])) {
+        header("HTTP/1.0 400 Bad Request");
+        print("Missing text");
+        exit();
+      }
+      
+      $text = trim($_REQUEST['text']);
+      if ($text == "") {
+        header("HTTP/1.0 400 Bad Request");
+        print("Bad text");
+        exit();
+      }
+
+      if (!isset($_REQUEST['parentID'])) {
+        header("HTTP/1.0 400 Bad Request");
+        print("Missing parentID");
+        exit();
+      }
+      
+      $datetime= date('m/d/Y h:i:s a');
+      $weight = 1;
+      $parentID = trim($_REQUEST['parentID']);
+
+      $new_comment = Comment::create($pid, $uid, $text, $datetime, $weight, $parentID);
+
+      if ($new_comment == null) {
+        header("HTTP/1.0 500 Server Error");
+        print("Server couldn't create new comment.");
+        exit();
+      }
+      
+      header("Content-type: application/json");
+      print($new_comment->getJSON());
+      exit();
+  }if(sizeof($path_components)>=2 && $path_components[1]!=""){
+      $cid= intval($path_components[1]);
+    
+      if (!isset($_REQUEST['uid'])) {
+        header("HTTP/1.0 400 Bad Request");
+        print("Missing user");
+        exit();
+      }
+      
+      $uid = trim($_REQUEST['uid']);
+      if ($uid == "") {
+        header("HTTP/1.0 400 Bad Request");
+        print("Bad user");
+        exit();
+      }
+
+      if (!isset($_REQUEST['title'])) {
+        header("HTTP/1.0 400 Bad Request");
+        print("Missing title");
+        exit();
+      }
+      
+      $title = trim($_REQUEST['title']);
+      if ($title == "") {
+        header("HTTP/1.0 400 Bad Request");
+        print("Bad title");
+        exit();
+      }
+
+      if (!isset($_REQUEST['text'])) {
+        header("HTTP/1.0 400 Bad Request");
+        print("Missing text");
+        exit();
+      }
+      
+      $text = trim($_REQUEST['text']);
+      if ($text == "") {
+        header("HTTP/1.0 400 Bad Request");
+        print("Bad text");
+        exit();
+      }
+      
+      $datetime= date('m/d/Y h:i:s a');
+      $weight = 1;
+
+      $new_post = Post::create($cid, $uid, $title, $text, $datetime, $weight);
+
+      if ($new_todo == null) {
+        header("HTTP/1.0 500 Server Error");
+        print("Server couldn't create new post.");
+        exit();
+      }
+      
+      header("Content-type: application/json");
+      print($new_post->getJSON());
+      exit();
+    }
+} else if ($_SERVER['REQUEST_METHOD'] == "PUT") {
+    //updating comment
+  if (sizeof($path_components)>=4 && $path_components[3]!=""&& $path_components[2]!=""&& $path_components[1]!="") {
+      $coid= intval($path_components[3]);
+      $comment = Comment::findByCOID($coid);
+
+      if ($comment == null) {
+        header("HTTP/1.0 404 Not Found");
+        print("comment id: " . $coid . " not found.");
+        exit();
+      }
+      
+      $upvote=false;
+      if(isset($_REQUEST['upvote'])){
+          $upvote=trim($_REQUEST['upvote']);
+      }
+
+      if($upvote){
+          $comment->upvoteComment();
+          print($comment->getJSON());
+          exit();
+      }
+
+      $downvote=false;
+      if(isset($_REQUEST['downvote'])){
+          $downvote=trim($_REQUEST['downvote']);
+      }
+
+      if($downvote){
+          $comment->downvoteComment();
+          print($comment->getJSON());
+          exit();
+      }
+
+      $newText = false;
+      if (isset($_REQUEST['text'])) {
+        $newText = trim($_REQUEST['text']);
+      }
+
+      if ($newText != false) {
+        $comment->setText($newText);
+        $time= date('m/d/Y h:i:s a');
+        $comment->setDatetime($time);
+        print($comment->getJSON());
+      }
+      exit();
+
+   //updating post   
+   }else if (sizeof($path_components)>=3 && $path_components[2]!=""&& $path_components[1]!="") {
+      $pid= intval($path_components[2]);
+      $post = Post::findByPID($pid);
+
+      if ($post == null) {
+        header("HTTP/1.0 404 Not Found");
+        print("post id: " . $pid . " not found.");
+        exit();
+      }
+
+      $upvote=false;
+      if(isset($_REQUEST['upvote'])){
+          $upvote=trim($_REQUEST['upvote']);
+      }
+
+      if($upvote){
+          $post->upvotePost();
+          print($post->getJSON());
+          exit();
+      }
+
+      $downvote=false;
+      if(isset($_REQUEST['downvote'])){
+          $downvote=trim($_REQUEST['downvote']);
+      }
+
+      if($downvote){
+          $post->downvotePost();
+          print($post->getJSON());
+          exit();
+      }
+
+      $newText = false;
+      if (isset($_REQUEST['text'])) {
+        $newText = trim($_REQUEST['text']);
+      }
+
+      if ($newText != false) {
+        $post->setText($newText);
+        $time= date('m/d/Y h:i:s a');
+        $post->setDatetime($time);
+        print($post->getJSON());
+      }
+
+      exit();
+  }
+}else if ($_SERVER['REQUEST_METHOD'] == "Delete") {
+  if (sizeof($path_components)>=4 && $path_components[3]!=""&& $path_components[2]!=""&& $path_components[1]!="") {
+      $coid= intval($path_components[3]);
+      $comment = Comment::findByCOID($coid);
+
+      if ($comment == null) {
+        header("HTTP/1.0 404 Not Found");
+        print("comment id: " . $coid . " not found.");
+        exit();
+      }
+
+      $comment->deleteComment();
+      exit();
+   }else if (sizeof($path_components)>=3 && $path_components[2]!=""&& $path_components[1]!="") {
+      $pid= intval($path_components[2]);
+      $post = Post::findByPID($pid);
+
+      if ($post == null) {
+        header("HTTP/1.0 404 Not Found");
+        print("post id: " . $pid . " not found.");
+        exit();
+      }
+
+      $post->deletePost();
+      exit();
+  }
+}
+
---
+++ Database/orm.php
@@ -0,0 +1,302 @@
+<?PHP
+
+date_default_timezone_set('America/New_York');
+
+class Post{
+
+    private $pid;
+    private $cid;
+    private $uid;
+    private $title;
+    private $text;
+    private $datetime;
+    private $weight;
+    private $com;
+
+    private function __construct($pid, $cid, $uid, $title, $text, $datetime,$weight, $com) {
+        $this->pid = $pid;
+        $this->cid = $cid;
+        $this->uid = $uid;
+        $this->title = $title;
+        $this->text = $text;
+        $this->datetime = $datetime;
+        $this->weight = $weight;
+        $this->com = $com;
+    }
+
+    public static function connect() {
+      return new SQLite3('websiteDatabase.db');
+    }
+
+    public static function findByCID($cid){
+      if($cid>47){
+        return "No class with cid '$cid'";
+      }
+      $conn= Post::connect();
+      $result = $conn->query("select * from Post p where p.cid='$cid' ORDER BY p.weight DESC, p.Datetime DESC");
+      $posts=array();
+
+      if ($result) {
+        while ($next_row = $result->fetchArray()) {
+            $post=array('pid'=> $next_row[0], 'cid'=> $next_row[1], 'uid'=> $next_row[2], 'title'=> $next_row[3],
+                          'text'=> $next_row[4], 'datetime'=> $next_row[5], 'weight'=>$next_row[6]) ;
+            $posts[]=$post;
+        }
+      }
+      return json_encode($posts, JSON_PRETTY_PRINT);
+    }
+
+    public static function findByPID($pid){
+      $conn= Post::connect();
+      $postResult = $conn->query("select * from Post p where p.pid='$pid'");
+      $page=array('post'=>"");
+      $post=array();
+      while ($next_row = $postResult->fetchArray()) {
+            $post=array('pid'=> $next_row[0], 'cid'=> $next_row[1], 'uid'=> $next_row[2], 'title'=> $next_row[3],
+                          'text'=> $next_row[4], 'datetime'=> $next_row[5], 'weight'=>$next_row[6]) ;
+            $page['post']=$post;
+      }if($page['post']==""){return null;}
+
+      $commentResult = $conn->query("select * from Comment c where c.pid='$pid' Order by c.weight DESC, c.Datetime DESC");
+      $comments=array();
+      $childComments=Comment::childCommentIDS();
+      while ($next_row = $commentResult->fetchArray()) {
+            if(in_array($next_row[0], $childComments)){
+              continue;
+            }
+            $comment=array('CoID'=> $next_row[0], 'pid'=> $next_row[1], 'uid'=> $next_row[2], 'text'=> $next_row[3],
+                          'datetime'=> $next_row[4], 'weight'=> $next_row[5]);
+            $replies=Comment::findChildren($next_row[0]);
+            $comment["replies"]=$replies;
+            $comments[]=$comment;
+      }
+      $page['comments']=$comments;
+
+      return new Post($post['pid'],
+                          $post['cid'],
+                          $post['uid'],
+                          $post['title'],
+                          $post['text'],
+                          $post['datetime'],
+                          $post['weight'],
+                          $comments);
+    }
+
+    public function getJSON() {
+    $json_obj = array('pid' => $this->pid,
+          'cid' => $this->cid,
+          'uid' => $this->uid,
+          'title'=> $this->title,
+          'text'=> $this->text,
+          'datetime'=> $this->datetime,
+          'weight'=> $this->weight,
+          'comments'=> $this->com);
+    return json_encode($json_obj, JSON_PRETTY_PRINT);
+  }
+
+  public function setText($text) {
+    $this->text = $text;
+    return $this->update();
+  }
+
+  public function setDatetime($datetime){
+    $this->datetime = $datetime;
+    return $this->update();
+  }
+
+  private function update() {
+      $conn= Post::connect();
+
+      $result = $conn->query("update Post set " .
+             "text='" . SQLite3::escapeString($this->text) . "', " .
+             "datetime='" . $this->datetime . "', " .
+             "weight=" . $this->weight . 
+             " where pid=" . $this->pid);
+      return $result;
+  }
+
+  public static function create($cid, $uid, $title, $text, $datetime, $weight) {
+      $conn= Post::connect();
+      $result = $conn->query("insert into Post(cid, uid, title, text, datetime, weight) values (" .
+            $cid . ", " .
+            $uid . ", " .
+            "'" . SQLite3::escapeString($title) . "', " .
+            "'" . SQLite3::escapeString($text) . "', " .
+            "'" . $datetime. "', " .
+            $weight.")");
+      
+      if ($result) {
+        $pid = SQLiteDatabase::lastInsertRowid('websiteDatabase.db');
+        $com=array();
+        return new Post($pid,$cid, $uid, $title, $text, $datetime, $weight, $com);
+      }
+      return null;
+  }
+
+  public function setWeight($weight){
+    $this->weight = $weight;
+    return $this->update();
+  }
+
+  public static function upvotePost(){
+    $updatedWeight= $this->weight;
+    $this->setWeight($updatedWeight+1);
+  }
+
+  public static function downvotePost(){
+    $updatedWeight= $this->weight;
+    $this->setWeight($updatedWeight-1);
+  }
+
+  public static function deletePost(){
+      $conn = Post::connect();
+      $conn->query("delete from Post where pid = " . $this->pid);
+  }
+}
+
+class Comment{
+
+    private $coid;
+    private $pid;
+    private $uid;
+    private $text;
+    private $datetime;
+    private $weight;
+    private $reply;
+
+    private function __construct($coid, $pid, $uid, $text, $datetime,$weight, $reply) {
+        $this->coid = $coid;
+        $this->pid = $pid;
+        $this->uid = $uid;
+        $this->text = $text;
+        $this->datetime = $datetime;
+        $this->weight = $weight;
+        $this->reply = $reply;
+    }
+
+    public static function connect() {
+        return new SQLite3('websiteDatabase.db');
+    }
+
+    public static function findByCOID($coid){
+        $conn= Comment::connect();
+        $commentResult = $conn->query("select * from Comment c where c.CoID='$coid'");
+        $comment=array();
+        while ($next_row = $commentResult->fetchArray()) {
+              $comment=array('CoID'=> $next_row[0], 'pid'=> $next_row[1], 'uid'=> $next_row[2], 'text'=> $next_row[3],
+                            'datetime'=> $next_row[4], 'weight'=> $next_row[5]);
+              $replies=Comment::findChildren($next_row[0]);
+              $comment["replies"]=$replies;
+        }
+        return new Comment($comment['CoID'],
+                          $comment['pid'],
+                          $comment['uid'],
+                          $comment['text'],
+                          $comment['datetime'],
+                          $comment['weight'],
+                          $comment['replies']);
+    }
+
+    public static function findChildren($CoID){
+        $conn= Comment::connect();
+        $childResult = $conn->query("select * from CommentHierarchy ch, Comment c where ch.comment='$CoID' AND c.CoID=ch.Child Order by c.weight DESC, c.Datetime DESC");
+        $reply=array();
+        $comments=0;
+        while ($next_row = $childResult->fetchArray()) {
+              $comments++;
+              $child=array('CoID'=> $next_row[2], 'pid'=> $next_row[3], 'uid'=> $next_row[4], 'text'=> $next_row[5],
+                            'datetime'=> $next_row[6], 'weight'=> $next_row[7]) ;
+              $nestedChildren = Comment::findChildren($next_row[1]);
+              $childComments[]=$next_row[2];
+              $child['replies']=$nestedChildren;
+              $reply[]=$child;
+        }
+        if($comments==0){
+          print "";
+        }
+        return $reply;
+    }
+
+    public static function childCommentIDS(){
+        $conn= Comment::connect();
+        $childResult = $conn->query("select c.CoID from comment c where c.CoID in (Select ch.Child from CommentHierarchy ch)");
+        $childIDS=array();
+        while ($next_row = $childResult->fetchArray()) {
+              $childIDS[]=$next_row[0];
+        }
+        return $childIDS;
+    }
+
+    public function getJSON() {
+    $json_obj = array('coid' => $this->coid,
+          'pid' => $this->pid,
+          'uid' => $this->uid,
+          'text'=> $this->text,
+          'datetime'=> $this->datetime,
+          'weight'=> $this->weight,
+          'replies'=> $this->reply);
+    return json_encode($json_obj, JSON_PRETTY_PRINT);
+  }
+
+  public function setText($text) {
+    $this->text = $text;
+    return $this->update();
+  }
+
+  public function setDatetime($datetime){
+    $this->datetime = $datetime;
+    return $this->update();
+  }
+
+  private function update() {
+      $conn= Comment::connect();
+
+      $result = $conn->query("update Comment set " .
+             "text='" . SQLite3::escapeString($this->text) . "', " .
+             "datetime='" . $this->datetime . "', " .
+             "weight=" . $this->weight . 
+             " where CoID=" . $this->coid);
+      return $result;
+  }
+
+  public static function create($pid, $uid, $text, $datetime, $weight, $parentID) {
+      $conn= Post::connect();
+      $result = $conn->query("insert into Comment(pid, uid, text, datetime, weight) values (" .
+            $pid . ", " .
+            $uid . ", " .
+            "'" . SQLite3::escapeString($text) . "', " .
+            "'" . $datetime. "', " .
+            $weight.")");
+      
+      if ($result) {
+        $coid = SQLiteDatabase::lastInsertRowid('websiteDatabase.db');
+        $reply=array();
+        $conn->query("insert into CommentHierarchy values (" .
+            $parentID . ", " .
+            $coid . ")");
+        return new Comment($coid, $pid,$cid, $uid, $title, $text, $datetime, $weight, $reply);
+      }
+      return null;
+  }
+
+  public function setWeight($weight){
+    $this->weight = $weight;
+    return $this->update();
+  }
+  
+  public static function upvoteComment(){
+    $updatedWeight= $this->weight;
+    $this->setWeight($updatedWeight+1);
+  }
+
+  public static function downvoteComment(){
+    $updatedWeight= $this->weight;
+    $this->setWeight($updatedWeight-1);
+  }
+
+  public static function deleteComment(){
+      $conn = Comment::connect();
+      $conn->query("delete from Comment where CoID = " . $this->coid);
+  }
+}
+?>
