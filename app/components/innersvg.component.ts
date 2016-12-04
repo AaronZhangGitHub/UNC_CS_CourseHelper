093226b0979569cfd4e070efab0f5ae97144581a
John Espenhahn <john@espenhahn.org>
2016-12-04 16:52:55 -0500

    SVG change color of taken courses

--- app/components/innersvg.component.ts
+++ app/components/innersvg.component.ts
@@ -3,6 +3,9 @@ import { CoursesService } from '../services/courses.service';
 import { CourseModel } from '../models/course.model';
 require("../../../lines/lines.js");
 
+// Force global
+var Snap = (window as any).Snap || function() {};
+
 // After user has created an account and selected their taken courses this is the default page
 @Component({
   selector: 'innersvg',
@@ -15,6 +18,14 @@ export class InnerSVGComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     (window as any).loadSVG();
+    
+    this.courseService.getTakenCourses().subscribe((courses: CourseModel[]) => {
+      for (let course of courses) {
+        var s = Snap("#lines-graphic");
+        var c = s.select(`g#cid-${course.CID}`);
+        if (c) c.attr({fill: "green"});
+      }
+    });
   }
   
   open(cid: number) {
