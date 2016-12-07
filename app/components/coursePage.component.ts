10bcc340c8a28e043eab4d82043dc631108d0f48
Nikhil Prasad <nikhil.prasad@hotmail.com>
2016-12-06 19:13:19 -0500

    began materializing forum

--- app/components/coursePage.component.ts
+++ app/components/coursePage.component.ts
@@ -19,17 +19,17 @@ template: `
       <a href = "#!class"><span class="white-text email" style = "text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;">Selected Course: {{ course }}</span></a>
     </div>
   </li>
-  <ul style = "overflow-y: auto;" class="collapsible" data-collapsible="accordion">
+  <ul style = "overflow-y: auto;" class="collapsible collapsible-accordion" data-collapsible="accordion">
     <li [class.active]="open">
       <div class="collapsible-header" (click)="open = !open" [class.active]="open"><i class="fa fa-book" aria-hidden="true"></i>Class</div>
       <div class="collapsible-body">
         <ul>
           <div style = "max-height:250px; overflow-y: auto;">
-            <a *ngFor="let course of courseService.getTakenCourses() | async" ng-class="{course.code: true}" href="javascript:void(0)" (click)="setCurrentUserClass(course.CID, course.code)">
-              <button class = "blue" style = "background-color:white; width:100%" type = "button" >
-              {{ course.code }}
-              </button>
-            </a>
+            <li style="cursor: pointer" class="collection-item" *ngFor="let course of courseService.getTakenCourses() | async" ng-class="{course.code: true}" href="javascript:void(0)" (click)="setCurrentUserClass(course.CID, course.code)">
+              <a>
+                {{ course.code }}
+              </a>
+            </li>
           </div>
         </ul>
       </div>
@@ -91,7 +91,9 @@ export class CoursePageComponent{
   }
   showForum(){
     if(this.course ==="Not Entered"){
-      alert("Please select a course first");
+      // alert("Please select a course first");
+      Materialize.toast('<span class="red-text">Please select a course first</span>', 5000);
+
     }else{
     this.forumB = true;
     this.recourcesB = false;
@@ -99,7 +101,8 @@ export class CoursePageComponent{
   }
   showResources(){
     if(this.course ==="Not Entered"){
-      alert("Please select a course first");
+      // alert("Please select a course first");
+       Materialize.toast('<span class="red-text">Please select a course first</span>', 5000);
     }else{
     this.forumB = false;
     this.recourcesB = true;
--- app/components/post.component.ts
+++ app/components/post.component.ts
@@ -11,9 +11,6 @@ import { UserService } from '../services/user.service';
 			<div class = "post-Info" style = "font-size: 8pt">
 				<div class = "post-title" style = "float: left; font-weight: bold;">&nbsp;Title: {{post.title}}</div>
 				<div class = "post-date" style = "float: left">&nbsp;Date-Posted: {{post.datetime}}</div>
-				<div class = "post-pid" style = "float: left">&nbsp;PID: {{post.pid}}</div>
-				<div class = "post-cid" style = "float: left">&nbsp;CID: {{post.cid}}</div>
-				<div class = "post-uid" style = "float: left">&nbsp;UID: {{post.uid}}</div>
 			</div>
 	    <div class = "post-Body" style = "font-size: 12pt">
 	    	<br>&nbsp;{{post.text}}
